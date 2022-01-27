5f2f802f410873f54635f7efbc0cbb3eaf7df342
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2022-01-27 22:53:17 +0100

    Banner improvements

--- src/components/Banner/Styles.js
+++ src/components/Banner/Styles.js
@@ -2,18 +2,25 @@ import styled, { css } from "styled-components";
 import GitHub from "@mui/icons-material/GitHub";
 import LinkedIn from "@mui/icons-material/LinkedIn";
 import Google from "@mui/icons-material/Google";
+import ArrowCircleDownOutlinedIcon from "@mui/icons-material/ArrowCircleDownOutlined";
 
 export const BannerContainer = styled.div`
+  position: relative;
+`;
+
+export const BannerWrapper = styled.div`
   height: 100vh;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   margin: 0 auto;
-  max-width: 1500px;
+  padding: 0 25px;
+  max-width: 1200px;
   max-height: 900px;
 
   @media (max-width: 768px) {
     flex-direction: column;
-    padding-top: 60px;
+    justify-content: flex-start;
+    padding: 60px 10px 0;
   }
 `;
 
@@ -22,29 +29,80 @@ export const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  max-width: 66%;
 
   @media (max-width: 768px) {
     text-align: center;
+    max-width: inherit;
+  }
+`;
+
+export const LargeTextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    margin-left: 30px;
   }
 `;
 
 export const TextLarge = styled.h1`
-  font-size: 32px;
-  font-weight: 700;
   display: block;
+  font-size: 48px;
+  font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 36px;
+  }
+`;
+
+export const Img = styled.img`
+  height: 48px;
+  object-fit: contain;
+  margin-left: 20px;
+
+  @media (max-width: 768px) {
+    height: 36px;
+  }
 `;
 
 export const TextMedium = styled.p`
   font-size: 24px;
+  font-weight: 300;
+  color: #8c8c8c;
+  display: block;
+
+  &:not(:last-child) {
+    margin-bottom: 5px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+export const SpanMedium = styled.span`
+  font-size: 26px;
   font-weight: 400;
+  color: ${(props) => props.color && props.color};
+
+  @media (max-width: 768px) {
+    font-size: 20px;
+    line-height: 1.5;
+  }
 `;
 
 export const MediumTextWrapper = styled.div`
-  padding: 10px 0;
+  margin-top: 10px;
 `;
 
 export const ButtonWrapper = styled.div`
-  margin-top: 10px;
+  margin-top: 25px;
+
+  @media (max-width: 768px) {
+    margin: 0 auto;
+  }
 `;
 
 export const ImageContainer = styled.div`
@@ -62,7 +120,11 @@ export const IconLinksWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 40px 0;
+  margin-top: 20px;
+
+  @media (max-width: 768px) {
+    margin: 10px 0;
+  }
 `;
 
 const iconStyle = css`
@@ -84,7 +146,7 @@ export const GoogleIcon = styled(Google)`
   ${iconStyle}
 `;
 
-export const Link = styled.a`
+export const LinkRef = styled.a`
   padding: 20px;
   cursor: pointer;
 
@@ -102,4 +164,20 @@ export const Link = styled.a`
     color: #db4a39;
     transition: ease-in-out 0.2s;
   }
+`;
+
+export const ScrollDownContainer = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 15px;
+  width: 100%;
+`;
+
+export const ScrollIcon = styled(ArrowCircleDownOutlinedIcon)`
+  color: gold;
+  height: 75px !important;
+  width: 75px !important;
+  cursor: pointer;
 `;
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import bannerImg from "./../../assets/banner_image.png";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { useWidth } from "./../../hooks";
+import bannerImg from "./../../assets/banner/banner_image.png";
+import waveDogImg from "./../../assets/banner/dog_wave.png";
 import Button from "../Button";
 import {
   BannerContainer,
@@ -10,45 +13,84 @@ import {
   TextContainer,
   TextLarge,
   TextMedium,
-  Wrapper,
   IconLinksWrapper,
-  Link,
   GitHubIcon,
   LinkedInIcon,
   GoogleIcon,
+  SpanMedium,
+  Img,
+  LargeTextWrapper,
+  BannerWrapper,
+  ScrollIcon,
+  LinkRef,
+  ScrollDownContainer,
 } from "./Styles";
 
 const Banner = () => {
+  const width = useWidth();
+
   return (
     <BannerContainer id="main">
-      <TextContainer>
-        <TextLarge>Hello there!</TextLarge>
+      <BannerWrapper>
+        <TextContainer>
+          <LargeTextWrapper>
+            <TextLarge>Hello there!</TextLarge>
+            <Img src={waveDogImg} alt="Waving dog" />
+          </LargeTextWrapper>
 
-        <MediumTextWrapper>
-          <TextMedium>My name is Patryk</TextMedium>
-          <TextMedium>I am a web developer from Poland</TextMedium>
-        </MediumTextWrapper>
+          <MediumTextWrapper>
+            <TextMedium>
+              My name is <SpanMedium color="#eebc1d">Patryk</SpanMedium>.
+            </TextMedium>
+            <TextMedium>
+              I'm a passionate, determined and hard working{" "}
+              <SpanMedium color="#6600cc">web developer</SpanMedium> from
+              Poland.
+            </TextMedium>
+            <TextMedium>
+              Always looking for new{" "}
+              <SpanMedium color="#cc0000">challenges</SpanMedium> and
+              <SpanMedium color="#cc00cc"> cooperation </SpanMedium>
+              opportunities.
+            </TextMedium>
+          </MediumTextWrapper>
 
-        <ButtonWrapper>
-          <Button primary>Contact me</Button>
-        </ButtonWrapper>
-      </TextContainer>
+          {width > "768" && (
+            <ButtonWrapper>
+              <Button primary>Contact me</Button>
+            </ButtonWrapper>
+          )}
+        </TextContainer>
 
-      <ImageContainer>
-        <Image src={bannerImg} />
+        <ImageContainer>
+          <Image src={bannerImg} alt="Banner image" />
 
-        <IconLinksWrapper>
-          <Link>
-            <GitHubIcon />
+          <IconLinksWrapper>
+            <LinkRef>
+              <GitHubIcon alt="Github icon" />
+            </LinkRef>
+            <LinkRef>
+              <LinkedInIcon alt="Linkedin icon" />
+            </LinkRef>
+            <LinkRef>
+              <GoogleIcon alt="Google icon" />
+            </LinkRef>
+          </IconLinksWrapper>
+
+          {width <= "768" && (
+            <ButtonWrapper>
+              <Button primary>Contact me</Button>
+            </ButtonWrapper>
+          )}
+        </ImageContainer>
+      </BannerWrapper>
+      {width > "768" && (
+        <ScrollDownContainer>
+          <Link to="skills" smooth={true} offset={-70}>
+            <ScrollIcon />
           </Link>
-          <Link>
-            <LinkedInIcon />
-          </Link>
-          <Link>
-            <GoogleIcon />
-          </Link>
-        </IconLinksWrapper>
-      </ImageContainer>
+        </ScrollDownContainer>
+      )}
     </BannerContainer>
   );
 };
